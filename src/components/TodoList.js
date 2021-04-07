c8a6d5bd03625c6cef52b1c4f8f3c0915380c951
jundogown <ns8708ns@naver.com>
2021-04-07 10:56:42 +0900

    [FIX] TodoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,14 +1,29 @@
-import react from 'react';
-import TodoListitem from './TodoListitem';
+import React from 'react';
+import styled from 'styled-components';
+import TodoItem from './TodoItem';
+import { useTodoState } from '../TodoContext';
+
+const TodoListBlock = styled.div`
+  flex: 1;
+  padding: 20px 32px;
+  padding-bottom: 48px;
+  overflow-y: auto;
+`;
 
 const TodoList = () => {
-    return(
-        <div className="TodoList">
-            <TodoListitem/>
-            <TodoListitem/>
-            <TodoListitem/>
-        </div>
-    )
+  const todos = useTodoState();
+  return (
+    <TodoListBlock>
+      {todos.map(todo => (
+        <TodoItem
+          id={todo.id}
+          text={todo.text}
+          done={todo.done}
+          key={todo.id}
+        />
+      ))}
+    </TodoListBlock>
+  );
 }
 
 export default TodoList;
